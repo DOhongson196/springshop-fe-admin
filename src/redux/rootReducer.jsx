887f92a7e080b045ee683f1a24a754d4e5b73516
DOhongson196 <109707579+DOhongson196@users.noreply.github.com>
2023-04-08 15:25:37 +0700

    Thêm và cấu hình Reducer, action cho Product

--- src/redux/rootReducer.jsx
+++ src/redux/rootReducer.jsx
@@ -1,13 +1,15 @@
 import { combineReducers } from "redux";
 import categoryReducer from "./reducers/categoryReducer";
 import commonReducer from "./reducers/commonReducer";
+import productReducer from "./reducers/productReducer";
 import manufacturerReducer from "./reducers/manufacturerReducer";
 
 
 const rootReducer = combineReducers({
     categoryReducer: categoryReducer,
-    commonReducer: commonReducer,
-    manufacturerReducer: manufacturerReducer,
+    commonReducer,
+    manufacturerReducer,
+    productReducer,
 })
 
 export default rootReducer;
